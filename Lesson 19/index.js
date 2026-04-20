a72d8029184eb4c7937dee4cc152eeae8d6e2f92
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-04-20 09:19:03 +0300

    Add files via upload

---
+++ Lesson 19/index.js
@@ -0,0 +1,11 @@
+// // let ism = prompt("Biror narsa kiriting")
+// // console.log(ism.length)
+// // console.log(ism.toUpperCase())
+// // let s = "JavaScript juda qiziqarli va kuchli dasturlash tilidir."
+// // console.log(s.includes("kuchli"))
+// // console.log(s.indexOf("juda"))
+// // let g = prompt("Elektron pochtangizni kiriting!")
+// // console.log(g.slice(13, 22))
+// let m = " Olma Behi Uzum  "
+// console.log(m.trim())
+// console.log(m.replace("Behi","Anor"))
