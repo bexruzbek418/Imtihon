aea9726ec95ecdb62a19210fe1469128d3fe4aaf
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-04-20 09:18:43 +0300

    Add files via upload

---
+++ Lesson 18/index.js
@@ -0,0 +1,4 @@
+let ism = "Bexruzbek, Kamron, Kamoladdin, Z.Muhammad, M.Muhammad, Farrux, Bekrahimbergan, Otabek, Barno, Mahliyo"
+console.log(ism + " " + ism.length)
+console.log(ism.indexOf("B"))
+console.log(ism[0])
