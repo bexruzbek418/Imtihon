26e9e4f74f8b8385e264fb55ee754940a1fd968c
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-04-21 21:14:44 +0500

    Add files via upload

---
+++ Lesson 20/style.js
@@ -0,0 +1,19 @@
+// let son = 256.789
+// console.log(son.toString())
+// console.log(son.toFixed(1))
+// console.log(son.toFixed(3))
+
+// let son = "45.6kg"
+// console.log(parseInt(son))
+// console.log(parseFloat(son))
+
+// let son = 7.9
+// console.log(Math.round(son))
+// console.log(Math.ceil(son))
+// console.log(Math.floor(son))
+
+// let son = -81
+// console.log(Math.abs(son))
+
+let son =
+console.log(Math.floor(Math.random() * 100))
