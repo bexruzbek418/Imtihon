08e05da4e32ce5c3f4f933bc0397711a84e5766d
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-04-22 11:37:36 +0500

    Add files via upload

---
+++ Lesson 21/index.js
@@ -0,0 +1,19 @@
+// let ism = "    aLISHER   "
+// ism = ism.trim()
+// ism = ism.toLowerCase()
+// ism = ism[0].toUpperCase() + ism.slice(1)
+// console.log(ism)
+
+// let e = "test@gmail.com"
+// if(e.includes("@") && 
+// e.includes(".") && 
+// !e.includes(" ") && 
+// e.indexOf("@")){
+//     console.log("true")
+// }
+
+// let phone = "+998 (90) 123-45-67";
+
+// let cleanPhone = phone.replace(/[^\d+]/g, "");
+
+// console.log(cleanPhone);
