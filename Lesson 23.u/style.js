85adcf620af150cf42f0564cc7a4e2a90451a477
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-05-06 10:35:42 +0500

    Add files via upload

---
+++ Lesson 23.u/style.js
@@ -0,0 +1,29 @@
+// function salomBer(s){
+//     return prompt
+// }
+// let ism = prompt("Ismingiz")
+// console.log("Assalomu alaykum! " + ism);
+
+// function sonlar(s){
+//     return prompt
+// }
+// let b = Number(prompt("Birinchi son"))
+// let i = Number(prompt("Ikkinchi son"))
+// console.log(b + i);
+
+// function sonlar(juft){
+//     if(son % 2 == 0){
+//         console.log("Bu son juft")
+//     }
+//     else{
+//         console.log("Bu son toq")
+//     }
+// }
+// let son = Number(prompt("Son kiriting"))
+// console.log(sonlar(son))
+
+function yil(son){
+    return prompt
+}
+let i = Number(prompt("Yilingizni yozing"))
+console.log("Siz " + Number(2026 - i) + " " + "yoshdasiz" )
