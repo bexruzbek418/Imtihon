9792bba16f4f786ee70ad2f9ed04ab748e3498f4
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-05-06 11:43:02 +0500

    Add files via upload

---
+++ Lesson 24/style.js
@@ -0,0 +1,57 @@
+// function salom(ism, vaqt){
+//     return prompt
+// }
+// let ism = prompt("Ismingiz")
+// let vaqt = prompt("Vaqti")
+// console.log("Xayrli " + vaqt + ", " + ism)
+
+// function son(s){
+//     return prompt
+// }
+// let i = prompt("Matn kiriting")
+// console.log(i + " " + i.length)
+
+// let i = prompt("Matn kiriting")
+// console.log(i.replaceAll("a", "@"))
+
+// const chegirma = (narx, foiz) => narx - (narx * foiz / 100)
+// let n = Number(prompt("Mahsulot narxi"))
+// let f = Number(prompt("Mahsulot foizi"))
+// console.log(chegirma(n, f))
+
+// function yetkazibBerish(summa){
+//     if(summa >= 10000){
+//         console.log("Yetkazib berish bepul")
+//     }
+//     else{
+//         console.log("Yetkazib berish pulli")
+//     }
+// }
+// console.log(yetkazibBerish(prompt("Mahsulot narxi")))
+
+// function muzqaymoqNarxi(soni){
+//     return soni * 5000
+// }
+// console.log(muzqaymoqNarxi(Number(prompt("Muzqaymoq nechta"))))
+
+// const vaqtniTop = (masofa, tezlik) => masofa / tezlik
+
+// let masofa = Number(prompt("Masofa qancha"))
+// let tezlik = Number(prompt("Tezlik qancha"))
+// console.log(vaqtniTop(masofa , tezlik))
+
+// function chiroqHolati(y){
+//     if(c == "yoq"){
+//         console.log("true")
+//     }
+//     else if(c == "o'chir'"){
+//         console.log("false")
+//     }
+// }
+// let c = prompt("Chiroq yoniqmi (yoq, o'chir)")
+// console.log(chiroqHolati(c))
+
+// function perimetr(uzunligi){
+//     return uzunligi * 4
+// } 
+// console.log(perimetr(Number(prompt("Kvadratning uzunligi nechchi"))))
