00499c9aa62fba47d6766e35ec1ba165a53f2d6a
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-05-08 10:54:52 +0500

    Add files via upload

---
+++ Lesson 24.u/style.js
@@ -0,0 +1,19 @@
+// function sharbatTayyorla(meva){
+//     return prompt
+// }
+// console.log(prompt("Mevani yozing") + " " + "sharbati")
+
+// function grammgaAylantir(kg){
+//     return kg * 1000
+// }
+// console.log(grammgaAylantir(Number(prompt("Kilogramni yozing"))) + " " + "gramm")
+
+// function haftalikJamgarm(pul){
+//     return pul * 7
+// }
+// console.log("Jami: " + haftalikJamgarm(Number(prompt("Pulni yozing"))))
+
+// function kattaHarf(ism){
+//     return ism.toUpperCase()
+// }
+// console.log(kattaHarf(prompt("Ismingizni yozing")))
