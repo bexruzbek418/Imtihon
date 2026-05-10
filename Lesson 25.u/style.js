b3cff4be172b520e385a2cfdc440c39244ac1779
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-05-10 22:22:35 +0500

    Add files via upload

---
+++ Lesson 25.u/style.js
@@ -0,0 +1,53 @@
+// let mashinalar = {
+//     rusum: "Chevrolet",
+//     model: "Malibu",
+//     yil:2023,
+//     rang:[
+//         "qora",
+//         "oq",
+//         "kulrang",
+//     ]
+// }
+// console.log(mashinalar.model)
+// console.log(mashinalar.rang[0])
+
+// let mashinalar = {
+//     rusum: "Chevrolet",
+//     model: "Malibu",
+//     yil:2023,
+//     rang:[
+//         "qora",
+//     ]
+// }
+// mashinalar.narx = 30000
+// mashinalar.rang[0] = "oq"
+// delete mashinalar.yil 
+// console.log(mashinalar)
+
+// let telefon = {
+//   marka:"Apple",
+//   model:"iPhone 15",
+//   xotira:"256 GB"
+// };
+// for (let kalit in telefon) {
+//   console.log(`${kalit} -> ${telefon[kalit]}`);
+// }
+
+// let kitob = { nom: "O'tgan kunlar",
+//     muallif: "Abdulla Qodiriy",
+//     sahifalar: 400 
+// }
+// console.log(Object.keys(kitob))
+// console.log(Object.values(kitob))
+// console.log(Object.entries(kitob))
+
+let xodim = {
+    ism: "Bekzod",
+    kasb: "marketing",
+    ish_joyi:{
+        manzil: "Mountain View",
+        kompaniya: "Google",
+    }
+}
+console.log(xodim)
+console.log(xodim.ish_joyi.kompaniya)
