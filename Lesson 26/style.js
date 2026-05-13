cdbfd7da9138ee65ba62a190d4b29ddbaf45856a
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-05-13 18:09:28 +0500

    Add files via upload

---
+++ Lesson 26/style.js
@@ -0,0 +1,37 @@
+// let mashinalar = ["BMW", "Tesla", "Mercedes"]
+// mashinalar.push("Audi")
+// mashinalar.unshift("Ferrari")
+// mashinalar.splice(1, 1)
+// console.log(mashinalar[mashinalar.length - 1])
+
+// let sonlar = [5, 10, 15, 20, 25];
+// for(son of sonlar){
+//     console.log(son * 2)
+// }
+
+// let baholar = [85, 42, 77, 90, 55, 30, 95];
+// let hammasi = baholar.filter(son => son > 60);
+// let katta = baholar.map(son => son + 5);
+// console.log(hammasi)
+// console.log(katta)
+
+// let mevalar = ["olma", "banan", "uzum", "anor"]
+
+// let meva = prompt("Meva nomini kiriting:")
+
+// if(mevalar.includes(meva)){
+//     console.log("Bizda bu meva bor")
+// } 
+// else{
+//    console.log("Afsuski, tugagan")
+// }
+
+// let raqamlar = [10, 20, 30, 40];
+
+// let yigindi = 0
+
+// for(son of raqamlar){
+//     yigindi += son
+// }
+
+// console.log(yigindi)
