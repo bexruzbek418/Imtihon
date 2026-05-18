e1a04cf7e67e953597b0e1915665ec9a694c667b
bexruzbek418 <bexruzbek.axv@gmail.com>
2026-05-18 09:36:06 +0300

    Add files via upload

---
+++ Lesson 27/style.js
@@ -0,0 +1,31 @@
+// for(let i = 1; i <= 5; i++){
+//     console.log("Narxi: " + i * 1000 + " " + "Soni: " + i)
+// }
+
+// let quvvat = 97
+// while(quvvat <= 100){
+//     console.log("Telefon quvvati: " + quvvat + "%")
+//     quvvat++
+// }
+
+// let muzqaymoq = false
+// do{
+//     console.log("Muzqaymoq qolmabdi")
+// }
+// while(muzqaymoq == true){
+//     console.log("Muzqaymoqni bir marta yedim")
+// }
+
+// for(let i = 1; i <= 5; i++){
+//     if(i == 3){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+for(let i = 6; i <= 10; i++){
+    if(i == 8){
+        break
+    }
+    console.log(i)
+}
